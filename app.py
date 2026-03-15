f70a539a051f157582a8104c74147f161384da34
valam75 <niranjan.praba1999@gmail.com>
2026-03-15 17:09:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "CI/CD Pipeline Working!"
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy"}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
